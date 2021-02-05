686f2f0263d0b1cde64fa3b9194f3dbed64c2a1c
Eduardo Castillo <ecsunahata@indracompany.com>
2021-02-05 01:11:34 -0500

    feat: se agrega config para evitar metodos post put y delete a las entidades

---
+++ src/main/java/com/eduardo/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.eduardo.ecommerce.config;
+
+import com.eduardo.ecommerce.entity.Product;
+import com.eduardo.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        // Disable HTTP methods for Product: POST, PUT, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // Disable HTTP methods for ProductCategory: POST, PUT, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}

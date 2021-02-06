e9f20682afaa7c8acf451ea0aea8b085f05b77b2
Eduardo Castillo <ecsunahata@indracompany.com>
2021-02-06 02:58:02 -0500

    feat: se agrega cors para que el servicio pueda consumirse desde el js del frontend

--- src/main/java/com/eduardo/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/eduardo/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.eduardo.ecommerce.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduardo.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }

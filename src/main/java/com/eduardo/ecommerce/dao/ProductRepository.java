ddc4bd867b61d2137e005fed3f0a8c16475a1d17
Eduardo Castillo <lincidro@gmail.com>
2021-02-25 17:42:20 -0500

    feat: se agrega el método para buscar productos por categoría

--- src/main/java/com/eduardo/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/eduardo/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,21 @@
 package com.eduardo.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduardo.ecommerce.entity.Product;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+    /**
+     * select * from product where category_id=?
+     * @param id, id to find on db
+     * @param pageable, is pageable
+     * @return
+     */
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
